3c8597216f35a1576dc267cd5d5291242aa8b4b6
EzoVn <vincent.galpin@gmail.com>
2022-01-21 16:05:21 +0100

    ajout de la page produit affichage OK

--- P5-Kanap/front/js/product.js
+++ P5-Kanap/front/js/product.js
@@ -1,7 +1,49 @@
 // Format des produits
 
-// class Product{
-//     constructor(jsonProduct){
-//         jsonProduct && Object.assign(this, jsonProduct);
-//     }
-// }
+class Product{
+    constructor(jsonProduct){
+        jsonProduct && Object.assign(this, jsonProduct);
+    }
+}
+
+// utiliser URLSearchparams pour extraire l'id depuis l'url et avec .get je cible ce qui se trouve apres id =
+const queryString = new URLSearchParams(document.location.search);
+const leId = queryString.get("id");
+
+console.log(leId);
+
+
+// id reccuperer maintgenant je doit aller le retrouver dans le tableau et afficher les informations correspondant au produit
+fetch("http://localhost:3000/api/products")
+    .then( res => res.json())
+    .then(jsonListProduct => {
+        for(let jsonProduct of jsonListProduct){
+            let product = new Product(jsonProduct);
+            // je parcours le tableau des produits si je trouve l'id correspondant a leId je copie l'element dans la variable produit
+            console.log(product);
+            
+            if(product._id == leId){
+                console.log('ok');
+                let produit = product;
+                console.log(produit);
+                return produit;
+            }else{
+                console.log('not ok');
+            }
+        }        
+    })
+    .then(produit => {
+        document.querySelector('.item__img').innerHTML += `<img src="${produit.imageUrl}" alt="${produit.altTxt}">`
+        document.getElementById('title').innerHTML += `${produit.name}`
+        document.getElementById('price').innerHTML += `${produit.price}`
+        document.getElementById('description').innerHTML += `${produit.description}`
+        // document.getElementById('colors').innerHTML += `<option value="${produit.colors[0]}">${produit.colors[0]}</option>`;
+        // document.getElementById('colors').innerHTML += `<option value="${produit.colors[1]}">${produit.colors[1]}</option>`;
+        // document.getElementById('colors').innerHTML += `<option value="${produit.colors[2]}">${produit.colors[2]}</option>`;
+        // faire une boucle pour charger chaque couleur dans le selecteur
+        produit.colors.forEach(function(element) {
+            let i = element;
+           document.getElementById('colors').innerHTML += `<option value="${element}">${element}</option>`;
+       });
+    })
+    
--- P5-Kanap/front/js/script.js
+++ P5-Kanap/front/js/script.js
@@ -10,7 +10,8 @@ fetch("http://localhost:3000/api/products")
     .then( jsonListProduct => {
         for(let jsonProduct of jsonListProduct){
             let product = new Product(jsonProduct);
-            document.querySelector('.items').innerHTML += `<a href="./product.html?id=42">
+            // pour retrouver l'id du produit j'ai ajouter id= dans le lien pour reccupérer ce qui se trouve après.
+            document.querySelector('.items').innerHTML += `<a href="./product.html?id=${product._id}">
             <article>
               <img src="${product.imageUrl}" alt="${product.altTxt}">
               <h3 class="productName">${product.name}</h3>
@@ -20,3 +21,4 @@ fetch("http://localhost:3000/api/products")
         }
     });
     
+   
